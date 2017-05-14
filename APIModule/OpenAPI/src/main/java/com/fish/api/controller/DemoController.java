5ebe9920ff719d29780a19c0b4d19ef2985fc280
fish <yudingm@163.com>
2017-05-14 23:08:05 +0800

    add demo and add Study notes

--- APIModule/OpenAPI/src/main/java/com/fish/api/controller/DemoController.java
+++ APIModule/OpenAPI/src/main/java/com/fish/api/controller/DemoController.java
@@ -1,6 +1,8 @@
 package com.fish.api.controller;
 
 import com.fish.controller.CommonController;
+import com.fish.service.ICallbackListener;
+import com.fish.service.ICallbackService;
 import com.fish.service.IDemoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,22 @@ public class DemoController extends CommonController {
     @Resource
     private IDemoService demoService;
 
+    @Resource
+    private ICallbackService callbackService;
+
     @RequestMapping(value = "sayHell", method = RequestMethod.POST)
     public void demo() {
         demoService.sayHell("zhasan");
+    }
+
+    @RequestMapping(value = "callback", method = RequestMethod.POST)
+    public void callback() {
+        callbackService.addListener("com.fish.www", new ICallbackListener() {
+            @Override
+            public void changed(String msg) {
+                System.out.println("callback1:" + msg);
+            }
+        });
     }
 
 
---
+++ ServerModule/ServerInterface/src/main/java/com/fish/service/ICallbackListener.java
@@ -0,0 +1,8 @@
+package com.fish.service;
+
+/**
+ * Created by fish on 2017/5/14.
+ */
+public interface ICallbackListener {
+    void changed(String msg);
+}
